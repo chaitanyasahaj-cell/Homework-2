811cb1b5871069a38c55851868d2fc5db3f07ea9
chaitanyasahaj-cell <chaitanyasahaj@gmail.com>
2026-09-23 20:04:47 +0100

    Update Game.py

--- Game.py
+++ Game.py
@@ -3,13 +3,29 @@ from pygame.locals import *
 pygame.init()
 
 WIDTH = 800
-HEIGHT = 800
+HEIGHT = 600
 
-screen = pygame.display.set_mode((800,800))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Recycle Project")
+
+def load_image(path, size):
+    return pygame.transform.scale(pygame.image.load(path).convert_alpha(), size)
+
+def draw_bg(image_name):
+    image_bg = load_image(image_name,(WIDTH, HEIGHT))
+    screen.blit(image_bg,(0,0))
+
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, img, size, x,y):
+        super().__init__()
+        self.image = load_image(img, size)
+        self.rect = self.image.get_rect(topleft = (x,y))
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            
+
+    draw_bg("Sahaj\\Games\\Recycle_Paper_Bag_NEW\\images\\bg.png")
+    pygame.display.update()
